3f353792cca061bbc6e0a362f8e66e4fed9d76c9
panzheng <panzheng@baidu.com>
2024-04-29 06:42:34 +0800

    读配置文件

---
+++ config/config.go
@@ -0,0 +1,33 @@
+// config/config.go
+package config
+
+import (
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Server struct {
+		Port int `yaml:"port"`
+	} `yaml:"server"`
+	Qianfan struct {
+		ApiKey    string `yaml:"apiKey"`
+		SecretKey string `yaml:"secretKey"`
+	} `yaml:"qianfan"`
+}
+
+var GlobalConfig Config
+
+func init() {
+	data, err := os.ReadFile("config.yaml")
+	if err != nil {
+		log.Fatalf("无法读取YAML文件: %v", err)
+	}
+
+	err = yaml.Unmarshal(data, &GlobalConfig)
+	if err != nil {
+		log.Fatalf("无法解析YAML文件: %v", err)
+	}
+}
--- main.go
+++ main.go
@@ -1,19 +1,26 @@
-package main  
-  
-import (  
-    "fmt"  
-    "log"  
-    "net/http"  
-)  
-  
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"capybara-go/config"
+)
+
 func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hello, world!")
+	fmt.Fprintln(w, "Hello, world!")
 }
-  
-func main() {  
-    http.HandleFunc("/", HelloWorldHandler)  
-    log.Println("Server starting on port 8080...")  
-    if err := http.ListenAndServe(":8080", nil); err != nil {  
-        log.Fatal("ListenAndServe: ", err)  
-    }  
+
+func chat() {
+
+}
+
+func main() {
+	port := config.GlobalConfig.Server.Port
+	http.HandleFunc("/", HelloWorldHandler)
+	log.Println(fmt.Sprintf("Server starting on port %d ...", port))
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
